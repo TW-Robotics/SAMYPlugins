4ae01da8d8107a64d83738ef0cdf48292401d2c6
michaelschebek <ma0180@technikum-wien.at>
2022-06-14 10:18:17 +0200

    start.sh erweitert um port für Roboter

--- SamyROS/start.sh
+++ SamyROS/start.sh
@@ -2,7 +2,7 @@
 echo "Starting ROS and sleeping for 5 seconds..."
 source /opt/ros/noetic/setup.bash 
 source ~/catkin_ws/devel/setup.bash 
-roslaunch dsr_launcher dsr_moveit_gazebo.launch ip:="$1" robotmode:="$2" &
+roslaunch dsr_launcher dsr_moveit_gazebo.launch robotip:="$1" robotport:="$2" robotmode:="$3" &
 sleep 5
 echo "Starting main.py"
 python3 main.py samycore doosan configFiles/ 
