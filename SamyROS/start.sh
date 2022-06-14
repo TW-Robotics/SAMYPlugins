e456269a047ea279c2953dc6dc340db4bd4f7575
michaelschebek <ma0180@technikum-wien.at>
2022-06-14 11:52:56 +0200

    added fourth arg for robot model

--- SamyROS/start.sh
+++ SamyROS/start.sh
@@ -2,7 +2,7 @@
 echo "Starting ROS and sleeping for 5 seconds..."
 source /opt/ros/noetic/setup.bash 
 source ~/catkin_ws/devel/setup.bash 
-roslaunch dsr_launcher dsr_moveit_gazebo.launch robotip:="$1" robotport:="$2" robotmode:="$3" &
+roslaunch dsr_launcher dsr_moveit_gazebo.launch robotip:="$1" robotport:="$2" robotmodel:="$3" robotmode:="$4" &
 sleep 5
 echo "Starting main.py"
 python3 main.py samycore doosan configFiles/ 
