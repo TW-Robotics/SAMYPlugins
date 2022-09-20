daf1c235a43bdef020b95f8e20a0b8ddc085cab4
Michael Schebek <ma0180@technikum-wien.at>
2022-09-20 10:20:30 +0200

    deleted old samyros folder

--- SamyROS/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting ROS and sleeping for 5 seconds..."
-source /opt/ros/noetic/setup.bash 
-source ~/catkin_ws/devel/setup.bash 
-roslaunch dsr_launcher dsr_moveit_gazebo.launch robotip:="$1" robotport:="$2" robotmodel:="$3" robotmode:="$4" &
-sleep 5
-echo "Starting main.py"
-python3 main.py samycore doosan configFiles/ 
