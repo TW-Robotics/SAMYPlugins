a0f47226f3dd3e0e48d2f021a009a928a861b776
michaelschebek <ma0180@technikum-wien.at>
2022-09-27 17:19:05 +0200

    new pics

--- KameraUsecasePlugin/kamera.py
+++ KameraUsecasePlugin/kamera.py
@@ -3,13 +3,12 @@ import cv2 as cv
 import numpy as np
 import pyrealsense2 as rs
 
-frame = cv.imread("Beispielbilder/1.jpg")
+frame = cv.imread("Beispielbilder/2.jpg")
+h,  w = frame.shape[:2]
 
-cv.namedWindow("img", 0)
-cv.imshow("img" , frame)
-cv.waitKey(1000)
-
-
+#cv.namedWindow("img", 0)
+#cv.imshow("img" , frame)
+#cv.waitKey(1000)
 
 kmat = [[1352.06955467799, 0, 973.803727404974] , [0, 1366.87675727465, 533.592541104211] , [0, 0, 1]]
 dstmat = [[0.127509519023764, -0.306871810182364, -0.000864384921776296, 0.00145553767677557, 0.125094379310040]]
@@ -17,12 +16,19 @@ kmat = np.array(kmat)
 dstmat = np.array(dstmat)
 kmat.reshape((3, 3))
 
+#newkmat, roi = cv.getOptimalNewCameraMatrix(kmat, dstmat, (w,h), 1, (w,h))
 undist = cv.undistort(frame, kmat, dstmat, None, None)
+
+
 
 cv.imshow("img" , undist)
 cv.waitKey(1000)
 
-cv.imwrite("undist.png" ,undist)
+
+
+
+
+#cv.imwrite("undist.png" ,undist)
 
 
 
